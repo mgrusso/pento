d89aa91d1af8b3d46610343bc75c951679dd9a20
Marco Russo <badtrap@gmail.com>
2024-12-20 17:09:36 +0100

    initial commit

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,63 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, score: 0, message: "Make a guess:", secret_no: Enum.random(1..10))}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1 class="mb-4 text-4xl font-extrabold">Your score: {@score}</h1>
+    <h2>
+      {@message}
+    </h2>
+    <br />
+    <h2>
+      <%= for n <- 1..10 do %>
+        <.link
+          class="bg-blue-500 hover:bg-blue-500
+        text-white font-bold py-2 px4 border border-blue-700 rounded m-1"
+          phx-click="guess"
+          phx-value-number={n}
+        >
+          {n}
+        </.link>
+      <% end %>
+    </h2>
+    """
+  end
+
+  def time() do
+    DateTime.utc_now() |> to_string
+  end
+
+  def handle_event("guess", %{"number" => guess}, socket) do
+    if String.to_integer(guess) == socket.assigns.secret_no do
+      message = "You won! Congratulations. I chose a new number!"
+      score = socket.assigns.score + 1
+      new_secret_no = Enum.random(1..10)
+
+      {
+        :noreply,
+        assign(
+          socket,
+          message: message,
+          score: score,
+          secret_no: new_secret_no
+        )
+      }
+    else
+      message = "Your guess: #{guess}- Wrong. Guess again. "
+      score = socket.assigns.score - 1
+
+      {
+        :noreply,
+        assign(
+          socket,
+          message: message,
+          score: score
+        )
+      }
+    end
+  end
+end
